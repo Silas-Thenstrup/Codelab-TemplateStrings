4b6c10cd875a650e79d1a026ad2337528123c9e4
Silas-Thenstrup <sini12@elev.techcollege.dk>
2025-09-02 09:24:10 +0200

    first 2 done

--- opgavefiler/js/firstPage.js
+++ opgavefiler/js/firstPage.js
@@ -2,13 +2,48 @@
 
 
 // din kode her
+        const navn = "Keld";
+        const aarIvirksomhed = 3;
+        const besked = `<h2>Hej ${navn}</h2>du har arbejdet i vores virksomhed i ${aarIvirksomhed} år.`;
+        console.log(besked);
 
+        let message = document.getElementById("message");
+        message.innerHTML = besked;
 
-/* Opgave 1*/
+/* Opgave 2*/
 
 
 // din kode her
+    const myData = [
+      { 
+        "navn": "T-shirt", 
+        "beskrivelse": "En t-shirt lavet af bomuld", 
+        "pris": 99 
+      },
+      { 
+        "navn": "Jeans", 
+        "beskrivelse": "Blå denim jeans med straight fit", 
+        "pris": 299 
+      },
+      { 
+        "navn": "Hættetrøje", 
+        "beskrivelse": "En grå hættetrøje med lynlås", 
+        "pris": 199 
+      }
+    ];
 
+    const produktListe = document.getElementById("produktListe");
+
+    myData.forEach(produkt => {
+      const template = `
+        <article class="produkt">
+          <h2>${produkt.navn}</h2>
+          <p>${produkt.beskrivelse}</p>
+          <p>Pris: ${produkt.pris} kr.</p>
+        </article>
+      `;
+      produktListe.innerHTML += template;
+    });
 
 
 /* Opgave 3 - skriv videre på koden her: */
